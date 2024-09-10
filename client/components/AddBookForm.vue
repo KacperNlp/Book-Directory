59c9a851f8bda115148d8ef0fc650c84eb1ba341
Kacper Nalepa <kacper.nlp@wp.pl>
2024-09-11 01:34:02 +0200

    Add element-plus UI library for FE and build basic FE structure

---
+++ client/components/AddBookForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card>
+    <template #header>
+      <div>
+        <h2>Card name</h2>
+      </div>
+    </template>
+    <div>
+      <p>Hello</p>
+    </div>
+    <template #footer>
+      <div>
+        <el-button type="primary">Submit</el-button>
+        <el-button type="warning">Reset</el-button>
+      </div>
+    </template>
+  </el-card>
+</template>
+
+<script lang="ts" setup></script>
